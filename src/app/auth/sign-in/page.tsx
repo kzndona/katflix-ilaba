a6906e60ce11838d5d46528dd395dc5084f7ae62
kzndona <165305757+kzndona@users.noreply.github.com>
2025-11-15 00:41:11 +0800

    - installed supabase-js package and supabase-ssr package - add supabase to env.local

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-sm bg-white shadow-xl rounded-2xl p-8">
-        <h1 className="text-2xl font-semibold text-center mb-6">
+        <h1 className="text-4xl font-semibold text-center mb-6">
           Ilaba Laundry Management
         </h1>
 
